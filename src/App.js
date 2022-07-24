102b0c785a5167e230488214faac2176ab65039e
Rufet <rufet.kerimli.2015@mail.ru>
2022-07-24 20:02:11 +0400

    added features

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
-import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
-import './App.css';
+import CourseGoalList from "./components/CourseGoals/CourseGoalList/CourseGoalList";
+import CourseInput from "./components/CourseGoals/CourseInput/CourseInput";
+import "./App.css";
 
 const App = () => {
   const [courseGoals, setCourseGoals] = useState([
-    { text: 'Do all exercises!', id: 'g1' },
-    { text: 'Finish the course!', id: 'g2' }
+    { text: "Do all exercises!", id: "g1" },
+    { text: "Finish the course!", id: "g2" },
   ]);
 
-  const addGoalHandler = enteredText => {
-    setCourseGoals(prevGoals => {
+  const addGoalHandler = (enteredText) => {
+    setCourseGoals((prevGoals) => {
       const updatedGoals = [...prevGoals];
       updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
       return updatedGoals;
     });
   };
 
-  const deleteItemHandler = goalId => {
-    setCourseGoals(prevGoals => {
-      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+  const deleteAll = (emptyArray) => {
+    setCourseGoals(emptyArray);
+  };
+
+  const deleteItemHandler = (goalId) => {
+    setCourseGoals((prevGoals) => {
+      const updatedGoals = prevGoals.filter((goal) => goal.id !== goalId);
       return updatedGoals;
     });
   };
-
+  
   let content = (
-    <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+    <p style={{ textAlign: "center" }}>No goals found. Maybe add one?</p>
   );
 
   if (courseGoals.length > 0) {
@@ -38,7 +42,7 @@ const App = () => {
   return (
     <div>
       <section id="goal-form">
-        <CourseInput onAddGoal={addGoalHandler} />
+        <CourseInput onDelete={deleteAll} onAddGoal={addGoalHandler} />
       </section>
       <section id="goals">
         {content}
--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,28 +1,46 @@
-import React, { useState } from 'react';
+import alertify from "alertifyjs";
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+import "alertifyjs/build/css/alertify.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const inputValue = enteredValue.trim().length;
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (inputValue === 0) {
+      alertify.error("Input cannot be empty", 1);
+      return;
+    }
+    alertify.success("Added", 2);
     props.onAddGoal(enteredValue);
   };
 
+  const deleteAll = () => {
+    props.onDelete("");
+  };
   return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
+    <div>
+      <form onSubmit={formSubmitHandler}>
+        <div className="form-control">
+          <label>Course Goal</label>
+          <input type="text" onChange={goalInputChangeHandler} />
+        </div>
+        <div className="main">
+          <Button type="submit">Add Goal</Button>
+          <Button type="button" onClick={deleteAll}>
+            Delete all
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 };
 
